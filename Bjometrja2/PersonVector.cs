d1092a406cea33ef4bc0f95c6d9383a66b4eaf54
Piotr Gornik <gornikp94@gmail.com>
2016-06-10 02:53:49 +0200

    kurwa ja kzwykle cośnie działą kolejny commit

---
+++ Bjometrja2/PersonVector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bjometrja2
+{
+    class PersonVector
+    {
+
+        public string id { get; set; }
+        public Dictionary<string, InputData> firstVector { get; set; }
+        public long distance { get; set; }
+
+        public PersonVector withId(string id)
+        {
+            this.id = id;
+            return this;
+        }
+
+        public PersonVector withFirstVector(Dictionary<string, InputData> firstVector)
+        {
+            this.firstVector = firstVector;
+            return this;
+        }
+      
+    }
+}
